9f5e2e0b91b799c51896df3be0320f8bbd5d0fa1
sbc <3403465822@qq.com>
2024-07-16 07:50:05 +0000

    lstm->mlp

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch
 from transformers import BertModel
 class Model(nn.Module):   
-    def __init__(self, lm:Union[BertModel,str], pad_token_id, query_lstm:Optional[Union[nn.Module,str]]=None, corpus_lstm:Optional[Union[nn.Module,str]]=None, corpus_batch_size = 6, bidirectional=False, device="cuda:0", lm_freezed=True,embedding_type="query"):
+    def __init__(self, lm:Union[BertModel,str], pad_token_id, query_lstm:Optional[Union[nn.Module,str]]=None, corpus_lstm:Optional[Union[nn.Module,str]]=None, corpus_batch_size = 6, bidirectional=False, device="cuda:0", lm_freezed=True,embedding_type="query",lstm_num_layers:int = 1, normalized:bool = False):
         super(Model, self).__init__()
         if isinstance(lm,str):
             self.lm = BertModel.from_pretrained(lm).to(device)
@@ -20,22 +20,22 @@ class Model(nn.Module):
         batch_first - If True, then the input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature). Note that this does not apply to hidden or cell states. See the Inputs/Outputs sections below for details. Default: False
         """
         if query_lstm is None:
-            self.query_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional,device = self.lm.device)
+            self.query_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional,device = self.lm.device,num_layers=lstm_num_layers,normalized=normalized)
         else:
             if isinstance(query_lstm,nn.Module):
                 self.query_lstm_layer = query_lstm.to(self.lm.device)
             else:
-                self.query_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional,  device = self.lm.device)
+                self.query_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional,  device = self.lm.device,num_layers=lstm_num_layers,normalized=normalized)
                 self.query_lstm_layer.load_state_dict(torch.load(query_lstm))
         self.query_lstm_layer.to(self.lm.device)
 
         if corpus_lstm is None:
-            self.corpus_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional, device = self.lm.device)
+            self.corpus_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional, device = self.lm.device,num_layers=lstm_num_layers,normalized=normalized)
         else:
             if isinstance(corpus_lstm,nn.Module):
                 self.corpus_lstm_layer = corpus_lstm.to(self.lm.device)
             else:
-                self.corpus_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional, device = self.lm.device)
+                self.corpus_lstm_layer = Encoder(num_inputs, self.num_hiddens, batch_first=True, bidirectional = bidirectional, device = self.lm.device,num_layers=lstm_num_layers,normalized=normalized)
                 self.corpus_lstm_layer.load_state_dict(torch.load(corpus_lstm))
         self.corpus_lstm_layer.to(self.lm.device)
 
@@ -118,13 +118,31 @@ class Model(nn.Module):
                         corpus_batch_size, self.num_hiddens), device=device))
                 
 class Encoder(nn.Module):
-    def __init__(self, num_inputs, num_hiddens, batch_first = True, bidirectional = False, device = "cuda:0", *args, **kwargs) -> None:
+    def __init__(self, num_inputs, num_hiddens, batch_first = True, bidirectional = False, device = "cuda:0",num_layers=1, normalized = False, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.normlized = normalized
         self.num_hiddens = num_hiddens
         self.num_directions = 2 if bidirectional else 1
         self.device = device
         # TODO: mlp & relu etc.
-        self.lstm_layter = nn.LSTM(num_inputs,self.num_hiddens,batch_first=batch_first)
+
+        self.lstm_layter = nn.LSTM(num_inputs,self.num_hiddens,batch_first=batch_first,num_layers=num_layers)
+        self.w1 = nn.Linear(256,512)
+        self.w2 = nn.Linear(512,256)
+        self.relu = nn.ReLU()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.LSTM):
+            for name, param in module.named_parameters():
+                if 'weight' in name:
+                    nn.init.xavier_uniform_(param)
+                elif 'bias' in name:
+                    nn.init.constant_(param, 0)
+        elif isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight,gain=nn.init.calculate_gain('relu'))
+            nn.init.constant_(module.bias, 0)
 
     def forward(self,input):
         """last_state (h_n,c_n)
@@ -132,10 +150,15 @@ class Encoder(nn.Module):
         batch_size = input.size(0)  # 从输入读取batch_size
         self.state = self.begin_state(batch_size = batch_size, device=self.device)
         _,last_state = self.lstm_layter(input,self.state)
+        vec = last_state[0].transpose(0,1).squeeze(1)
+        output = self.w2(self.relu(self.w1(vec)))
+        if self.normlized:
+            output = torch.nn.functional.normalize(output,dim=-1)
         if self.training:
-            return last_state[0].transpose(0,1).squeeze(1)
+            return output
         else:
-            return (last_state[0].transpose(0,1).repeat(1,input.shape[1],1),last_state[1])
+            return output
+            # return (last_state[0].transpose(0,1).repeat(1,input.shape[1],1),last_state[1])
 
     def begin_state(self, device, batch_size=1):
         if not isinstance(self.lstm_layter, nn.LSTM):
